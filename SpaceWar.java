7dbc4ef38cc4438c6594ea741891468a0b9b2f8e
golfsaetan <saetan.golf@gmail.com>
2016-04-13 18:53:18 +0700

    Added files via upload

---
+++ SpaceWar.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class SpaceWar {
+	public static void main(String[] args){
+		JFrame frame = new JFrame("Space War");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400, 650);
+		frame.getContentPane().setBackground(Color.black);
+		frame.setLocation(400,50);
+		frame.setVisible(true);
+	}
+}
